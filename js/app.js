7fcc4e48f86b132ac64f1edec1791acc2960af98
Alex Aguinaldo <psionicbones@gmail.com>
2017-03-13 08:30:27 -1000

    update

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("sanity check");
+
+//Finding HTML Elements
+// console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+var idTag = document.getElementsByTagName("script");
+console.log(idTag);
